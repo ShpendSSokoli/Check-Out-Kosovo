b4830c2afdf0158dee6cfc8cc8db0a23a4264e91
Kushtrim Mahmuti <kushtrimmahmuti@gmail.com>
2012-10-22 19:15:56 +0200

    sdf

---
+++ src/com/check/out/kosovo/List.java
@@ -0,0 +1,14 @@
+package com.check.out.kosovo;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class List extends ListActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list);
+	}
+}
